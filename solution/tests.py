24207e4a9b7210dbd9822d2492d176d6fab2f0f6
Albert <63642425+iamnalinor@users.noreply.github.com>
2024-03-04 23:54:37 +0400

    Add tests for 06, 08, 09 groups

--- solution/tests.py
+++ solution/tests.py
@@ -37,12 +37,12 @@ class TestResponse:
         print("assertStatus OK")
         return self
 
-    def assertJson(self, data: dict) -> Self:
+    def assertJson(self, data: "dict | json") -> Self:
         assert self.json == data
         print("assertJson OK")
         return self
 
-    def assertLambda(self, func: Callable[[dict], bool]) -> Self:
+    def assertLambda(self, func: Callable[["dict | json"], bool]) -> Self:
         assert func(self.json)
         print("assertLambda OK")
         return self
@@ -97,32 +97,32 @@ t.request("GET /countries/XX").assertError(404)
 
 t.request(
     "POST /auth/register",
-    json=register_set("test1", "johnwick@gmail.com", "123456", "US", True),
+    json=register_set("test1", "johnwick1@gmail.com", "123456", "US", True),
 ).assertError(400)
 
 t.request(
     "POST /auth/register",
-    json=register_set("test1", "johnwick@gmail.com", "abc1231321321", "US", True),
+    json=register_set("test1", "johnwick1@gmail.com", "abc1231321321", "US", True),
 ).assertError(400)
 
 t.request(
     "POST /auth/register",
-    json=register_set("test1", "johnwick@gmail.com", "Test123", "ZZ", True),
+    json=register_set("test1", "johnwick1@gmail.com", "Test123", "ZZ", True),
 ).assertError(400)
 
 t.request(
     "POST /auth/register",
-    json=register_set("**+**-*-*-", "johnwick@gmail.com", "Test123", "US", True),
+    json=register_set("**+**-*-*-", "johnwick1@gmail.com", "Test123", "US", True),
 ).assertError(400)
 
 t.request(
     "POST /auth/register",
-    json=register_set("1", "johnwick@gmail.com", "Test123!`", "RU", True),
+    json=register_set("1", "johnwick1@gmail.com", "Test123!`", "RU", True),
 ).assertStatus(201).assertJson(
     {
         "profile": {
             "login": prefix + "1",
-            "email": prefix + "johnwick@gmail.com",
+            "email": prefix + "johnwick1@gmail.com",
             "countryCode": "RU",
             "isPublic": True,
         },
@@ -131,7 +131,7 @@ t.request(
 
 t.request(
     "POST /auth/register",
-    json=register_set("2", "johnwick@gmail.com", "Test123", "RU", True),
+    json=register_set("2", "johnwick1@gmail.com", "Test123", "RU", True),
 ).assertError(409)
 
 t.request(
@@ -141,7 +141,7 @@ t.request(
 
 t.request(
     "POST /auth/register",
-    json=register_set("2", "johnwick@gmail.com", "Test123!`", "RU", True),
+    json=register_set("2", "johnwick1@gmail.com", "Test123!`", "RU", True),
 ).assertError(409)
 
 for i in range(2, 11):
@@ -158,21 +158,6 @@ for i in range(2, 11):
             },
         }
     )
-
-print(
-    t.request(
-        "POST /auth/sign-in", json={"login": f"{prefix}1", "password": "Test123!`"}
-    )
-    .assertStatus(200)
-    .json
-)
-print(
-    t.request(
-        "POST /auth/sign-in", json={"login": f"{prefix}1", "password": "Test123!`"}
-    )
-    .assertStatus(200)
-    .json
-)
 
 assert (
     t.request(
@@ -198,3 +183,82 @@ for i in range(1, 11):
         .json["token"]
     )
     t.accounts[str(i)] = token
+
+t.request("PATCH /me/profile", "1", json={"isPublic": False}).assertStatus(
+    200
+).assertJson(
+    {
+        "login": prefix + "1",
+        "email": prefix + f"johnwick1@gmail.com",
+        "countryCode": "RU",
+        "isPublic": False,
+    }
+)
+
+phone = f"+{random.randint(1, 999999999)}"
+
+t.request(
+    "PATCH /me/profile", "1", json={"isPublic": False, "phone": phone}
+).assertStatus(200).assertJson(
+    {
+        "login": prefix + "1",
+        "email": prefix + f"johnwick1@gmail.com",
+        "countryCode": "RU",
+        "isPublic": False,
+        "phone": phone,
+    }
+)
+
+t.request(
+    "PATCH /me/profile", "2", json={"isPublic": True, "phone": phone}
+).assertError(409)
+
+t.request("GET /me/profile", "2").assertStatus(200).assertJson(
+    {
+        "login": prefix + "2",
+        "email": prefix + f"johnwick2@gmail.com",
+        "countryCode": "BQ",
+        "isPublic": False,
+    }
+)
+
+t.request(f"GET /profiles/{prefix}1", "5").assertError(403)
+t.request(f"GET /profiles/{prefix}5", "1").assertError(403)
+t.request("POST /friends/add", "1", json={"login": prefix + "5"}).assertStatus(200)
+t.request("POST /friends/add", "1", json={"login": prefix + "5"}).assertStatus(200)
+t.request("POST /friends/add", "1", json={"login": "unknown"}).assertError(404)
+t.request("POST /friends/remove", "1", json={"login": prefix + "5"}).assertStatus(200)
+t.request("POST /friends/remove", "1", json={"login": prefix + "5"}).assertStatus(200)
+t.request("POST /friends/remove", "1", json={"login": "unknown"}).assertStatus(200)
+t.request("GET /friends", "1").assertStatus(200).assertJson([])
+t.request("POST /friends/add", "1", json={"login": prefix + "5"}).assertStatus(200)
+t.request("GET /friends", "1").assertStatus(200).assertLambda(
+    lambda f: len(f) == 1 and f[0]["login"] == prefix + "5" and f[0]["addedAt"]
+)
+
+t.request("GET /posts/0000-0000", "1").assertError(404)
+
+post_ids = []
+
+for i in range(105):
+    data = (
+        t.request(
+            "POST /posts/new", "1", json={"content": f"Post {i}", "tags": ["kek"]}
+        )
+        .assertStatus(200)
+        .assertLambda(
+            lambda p: p["content"] == f"Post {i}"
+            and p["tags"] == ["kek"]
+            and p["author"] == prefix + "1"
+            and p["id"]
+            and p["likesCount"] == 0
+        )
+        .json
+    )
+    t.request(f"GET /posts/{data['id']}", "1").assertStatus(200).assertJson(data)
+    post_ids.append(data["id"])
+
+    if i % 10 == 0:
+        t.request(f"GET /posts/{data['id']}", ["2", "3", "4"][i % 3]).assertError(404)
+
+t.request(f"GET /posts/{post_ids[0]}").assertError(401)
