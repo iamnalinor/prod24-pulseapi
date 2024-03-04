6dc3eb37a37698006b0ca8099631e45fef8be877
Albert <63642425+iamnalinor@users.noreply.github.com>
2024-03-04 19:51:46 +0400

    Remove condition on replacing

--- solution/project/env.py
+++ solution/project/env.py
@@ -2,8 +2,6 @@ from envparse import env
 
 __all__ = ["POSTGRES_CONN", "TOKEN_TTL_HOURS"]
 
-POSTGRES_CONN = env.str("POSTGRES_CONN")
-if "postgresql://" not in POSTGRES_CONN:
-    POSTGRES_CONN = POSTGRES_CONN.replace("postgres://", "postgresql://")
+POSTGRES_CONN = env.str("POSTGRES_CONN").replace("postgres://", "postgresql://")
 
 TOKEN_TTL_HOURS = 12
